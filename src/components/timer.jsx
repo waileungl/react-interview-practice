35fcf4c37ee82e857a911c06f90ed73bfd57c982
William Liu <110699040+waileungl@users.noreply.github.com>
2023-03-02 13:19:51 -0800

    update count down base on OA

--- src/components/timer.jsx
+++ src/components/timer.jsx
@@ -1,29 +1,83 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState, Fragment } from 'react';
 
 const Timer = () => {
-    const [isPause, setIsPause] = useState(false)
-    const [count, setCount] = useState(10)
+    const [toggle, setToggle] = useState(false)
+    const [timer, setTimer] = useState(0)
+    const [min, setMin] = useState(false)
+    const [sec, setSec] = useState(false)
 
 
     useEffect(() => {
-        let countDown;
-        if (count > 0 && !isPause) {
-            countDown = setInterval(() => {
-                setCount(count - 1)
+        let counter;
+        if (toggle) {
+            counter = setInterval(() => {
+                setTimer(timer => timer - 1)
             }, 1000)
         }
+        return () => {
+            clearInterval(counter)
+        }
+    }, [toggle])
 
-        return () => clearInterval(countDown)
-    }, [count, isPause])
+    useEffect(() => {
+        if (timer <= 0) setToggle(false)
+    }, [timer])
+
+    const handleStart = () => {
+        setToggle(true);
+        setTimer(Number(min * 60) + Number(sec))
+        // setCounter(setInterval(() => setTimer(timer => timer - 1), 1000));
+    };
+    const handlePR = () => {
+        // if(toggle) {
+        //   clearInterval(counter)
+        // } else {
+        //   setCounter(setInterval(() => setTimer(timer => timer - 1), 1000));
+        // }
+        setToggle(toggle => !toggle);
+    }
+    const handleReset = () => {
+        setMin(0);
+        setSec(0);
+        setTimer(0);
+        setToggle(false);
+        // if(counter) clearInterval(counter);
+    }
+    const showMin = Math.floor(timer / 60);
+    const showSec = timer % 60
 
     return (
-        <>
-            {count}
-            <button onClick={() => setIsPause(!isPause)}>{isPause ? "Resume" : "Pause"}</button>
+        <Fragment>
+            <label>
+                <input
+                    value={min}
+                    onChange={(e) => { setMin(e.target.value) }}
+                    type="number"
+                />
+                Minutes
+            </label>
+            <label>
+                <input
+                    value={sec}
+                    onChange={(e) => { setSec(e.target.value) }}
+                    type="number"
+                />
+                Seconds
+            </label>
 
-            <iframe src="https://devbud.wlliu.com/" frameborder="0" height={800} width={1200}></iframe>
-        </>
-    )
+            <button onClick={handleStart}>START</button>
+            <button onClick={handlePR}>PAUSE / RESUME</button>
+            <button onClick={handleReset}>RESET</button>
+
+            <h1 data-testid="running-clock">
+                {
+                    showMin < 10 ? "0" + showMin : showMin
+                }:{
+                    showSec < 10 ? "0" + showSec : showSec
+                }
+            </h1>
+        </Fragment>
+    );
 }
 
 export default Timer;
